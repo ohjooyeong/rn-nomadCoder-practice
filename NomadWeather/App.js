d3b636c0b836563f283b6808ebd44cfe27bf5b54
OH-JOO-YEONG <brb1111@naver.com>
2022-06-26 00:26:21 +0900

    [Feat] Weather 기능 구현 및 적용

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+    Dimensions,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    ActivityIndicator,
+} from "react-native";
 import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "bfc15a948dba0213f8f367bf9586ec87";
+
 export default function App() {
     const [city, setCity] = useState("Loading...");
-    const [location, setLocation] = useState(null);
+    const [days, setDays] = useState([]);
     const [ok, setOk] = useState(true);
 
-    const ask = async () => {
+    const getWeather = async () => {
         const { granted } = await Location.requestForegroundPermissionsAsync();
         if (!granted) {
             setOk(false);
@@ -24,10 +33,19 @@ export default function App() {
             { useGoogleMaps: false }
         );
         setCity(location[0].region);
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+        );
+        const json = await response.json();
+
+        if (json.daily) {
+            setDays(json.daily);
+        }
     };
 
     useEffect(() => {
-        ask();
+        getWeather();
     }, []);
 
     return (
@@ -41,22 +59,29 @@ export default function App() {
                 pagingEnabled
                 showsHorizontalScrollIndicator={false}
             >
-                <View style={styles.day}>
-                    <Text style={styles.temp}>24</Text>
-                    <Text style={styles.desc}>Sunny</Text>
-                </View>
-                <View style={styles.day}>
-                    <Text style={styles.temp}>24</Text>
-                    <Text style={styles.desc}>Sunny</Text>
-                </View>
-                <View style={styles.day}>
-                    <Text style={styles.temp}>24</Text>
-                    <Text style={styles.desc}>Sunny</Text>
-                </View>
-                <View style={styles.day}>
-                    <Text style={styles.temp}>24</Text>
-                    <Text style={styles.desc}>Sunny</Text>
-                </View>
+                {days.length === 0 ? (
+                    <View style={styles.day}>
+                        <ActivityIndicator
+                            color={"white"}
+                            size="large"
+                            style={{ marginTop: 10 }}
+                        />
+                    </View>
+                ) : (
+                    days.map((day, index) => (
+                        <View style={styles.day} key={index}>
+                            <Text style={styles.temp}>
+                                {parseFloat(day.temp.day).toFixed(1)}
+                            </Text>
+                            <Text style={styles.desc}>
+                                {day.weather[0].main}
+                            </Text>
+                            <Text style={styles.tinyText}>
+                                {day.weather[0].description}
+                            </Text>
+                        </View>
+                    ))
+                )}
             </ScrollView>
         </View>
     );
@@ -73,17 +98,23 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
     cityName: {
-        color: "black",
+        color: "#fff",
         fontSize: 53,
     },
-    weather: {},
+    weather: { color: "#fff" },
     day: {
         width: SCREEN_WIDTH,
         alignItems: "center",
     },
     temp: {
         marginTop: 50,
-        fontSize: 178,
+        fontSize: 128,
+        color: "#fff",
     },
-    desc: { fontSize: 60, marginTop: -30 },
+    desc: { fontSize: 60, marginTop: -20, color: "#fff" },
+
+    tinyText: {
+        color: "#fff",
+        fontSize: 20,
+    },
 });
